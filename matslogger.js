f7a17ec51300ef224b4d29c2d134de5ff63eeba4
Matito <matitoroberto@hotmail.co.nz>
2016-01-02 21:42:56 +1100

    Basic functions added. Not yet tested.

---
+++ matslogger.js
@@ -0,0 +1,177 @@
+// matslogger.js
+
+function MatsLogger() {
+
+	// enviroment enum
+	const DEV_ENVIRO = 'development';
+	const TEST_ENVIRO = 'test';
+	const PROD_ENVIRO = 'production';
+
+	// default logger names
+	const WARN = 'warn';
+	const DEBUG = 'debug';
+	const INFO = 'info';
+
+	var logger = this;			// keep a ref to this
+	var _enviroment = null;		// private enviroment variable
+	var _udls = {}, _udl_state = {};	// objects to hold the User Defined Logging (UDL) functions and states
+	this.config = config = {};	// expose config object
+	this.hook = hook = {};		// object to allow hooks to be inserted
+
+	function invalid_enviroment() {
+		throw new Error('Invalid enviroment variable given.');
+	}
+
+	function validate_logging_function(loggingFunction) {
+		if (loggingFunction || typeof loggingFunction !== 'function') {
+			throw new Error('Invalid logging function given.');
+		}
+		else {
+			return true;
+		}
+	} 
+
+	function valid_logging_function(enviro, loggingFunction) {
+		if (enviro !== DEV_ENVIRO || enviro !== TEST_ENVIRO || enviro !== PROD_ENVIRO) {
+			invalid_enviroment();
+		}
+		else {
+			return validate_logging_function(replacementFunction);
+		}
+	}
+
+	// set's the enviroment variable
+	config.enviroment = function (enviro) {
+		if (_enviroment) {
+			throw new Error('Enviroment already set.  Cannot reset enviroment setting.');
+		}
+		else {
+			if (enviro === DEV_ENVIRO || enviro === TEST_ENVIRO || enviro === PROD_ENVIRO) {
+				_enviroment = enviro;
+			}
+			else {
+				invalid_enviroment();
+			}
+		}
+	};
+
+	// allow replacement functions to defaults
+	config.warn = function (enviro, replacementFunction) {
+		// test that a vaild enviroment was specified
+		if (valid_logging_function(enviro, replacementFunction)) {
+			logger.warn = replacementFunction;
+		}
+	};
+
+	config.debug = function (enviro, replacementFunction) {
+		// test that a vaild enviroment was specified
+		if (valid_logging_function(enviro, replacementFunction)) {
+			logger.debug = replacementFunction;
+		}
+	};
+
+	config.info = function (enviro, replacementFunction) {
+		// test that a vaild enviroment was specified
+		if (valid_logging_function(enviro, replacementFunction)) {
+			logger.info = replacementFunction;
+		}
+	};
+
+	// allow hooks into the standard functions
+	config.hook = function (name, hookFunction) {
+		if (!name || name !== WARN || name !== DEBUG || name !== INFO) {
+			// no valid name fond
+			throw new Error('Please specify a valid logging function to hook into.');
+		}
+		if (validate_logging_function(hookFunction)) {
+			var new_function;
+				// result return by hook function is processed by the default function
+				// this allows a hook function to extend or replace the default function
+			swtich (name){
+				case WARN:
+					new_function = function (message) {
+						log_warn(hookFunction.call(logger, message));
+					};
+					break;
+				case DEBUG:
+					new_function = function (message) {
+						log_debug(hookFunction.call(logger, message));
+					};
+					break;
+				case INFO:
+					new_function = function (message) {
+						log_info(hookFunction.call(logger, message));
+					};
+					break;
+				// fallthrough to nothing
+			}
+			// set our new extended function to the logger object
+			if (new_function && typeof new_function == 'function') {
+				logger[name] = new_function;
+			}
+		}
+	};
+
+	// allow users to define their own functions with custom names
+	config.udl = function (name, loggingFunction) {
+		// validation
+		if (!name || typeof name !== 'string') {
+			throw new Error('Please specify a valid name for the User Defined Logging function.');
+		}
+		else {
+			if (loggingFunction || typeof loggingFunction !== 'function') {
+				throw new Error('Please specifiy a valid logging function.');
+			}
+			else {
+				_udl_state[name] = false;	// turn off when created
+				logger[name] = function (message) {
+					if (_udl_state[name]) {
+						// logging is set for this UDL fun
+						loggingFunction.call(logger, message);
+					}
+				};
+			}
+		}
+	};
+
+	config.on = function (name) {
+		// turn on function by name
+		if (name && _udl_state[name] !== undefined) {
+			_udl_state[name] = true;
+		}
+	};
+	
+	// default logging functionality
+	function log_warn(message) {
+		// always log unless overridden
+		if (message) {
+			console.log(message);
+		}
+	};
+	this.warn = log_warn;
+
+	function log_debug(message) {
+		if (!message) {
+			return false;
+		}
+		// only log if right enviroment
+		if (_enviroment === PROD_ENVIRO || _enviroment === TEST_ENVIRO) {
+			console.log(message);
+		}
+	};
+	this.debug = log_debug;
+
+	function log_info(message) {
+		if (!message) {
+			return false;
+		}
+		// log only in dev
+		if (_enviroment === DEV_ENVIRO) {
+			console.log(message);
+		}
+	};
+	this.info = log_info;
+
+}
+
+module.exports = new MatsLogger();
